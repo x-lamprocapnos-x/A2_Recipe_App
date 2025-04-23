4f189c1bb3dfe5b123050a886ddc43ada2e69ef2
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-04-23 15:42:27 -0400

    create recipe model for recipe app, register the model to admin.py, and run migrations

---
+++ src/recipes/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    ingredients = models.CharField(
+        max_length=225,
+        help_text="Enter the ingredients, seperated by a comma"
+        )
+    cooking_time = models.IntegerField(help_text='Time in minutes')
+
+    # calculate difficulty
+    def calculate_difficulty(self):
+        ingredients = self.ingredients.split(', ')
+        if self.cooking_time < 10 and len(ingredients) < 4:
+            return 'Easy'
+        elif self.cooking_time < 10 and len(ingredients) >= 4:
+            return 'Intermediate'
+        elif self.cooking_time < 15 and len(ingredients) >= 4:
+            return 'Moderate'
+        else:
+            return 'Hard'
+
+    def __str__(self):
+        return self.name
+   

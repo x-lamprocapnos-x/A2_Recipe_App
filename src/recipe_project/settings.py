05398f805ce5b885e23ecdfb2f8faeb2250bf9e5
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-04-23 15:41:08 -0400

    create recipe app for recipe project

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #recipe_project related apps
+    'recipes'
 ]
 
 MIDDLEWARE = [

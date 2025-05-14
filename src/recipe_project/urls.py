d177a84e2c92ec75fc6636ddee48ceebdd503771
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-05-13 21:00:06 -0400

    Update root urls.py to setup proper direction for media files

--- src/recipe_project/urls.py
+++ src/recipe_project/urls.py
@@ -22,3 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

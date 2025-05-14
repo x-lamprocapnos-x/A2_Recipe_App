628ebdb25c5692b4a1fe5d72e58b638d65b91dd8
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-05-13 20:00:45 -0400

    add tests to test, the different fields in the recipe model, add a test to make sure the difficulty saves, add a user in test data, update al urls to include 'recipe:" in url tests

--- src/recipes/tests.py
+++ src/recipes/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from .models import Recipe
 
 # Create your tests here.
@@ -10,11 +11,13 @@ class RecipeModelTest(TestCase):
 
     # Test data
     def setUp(self):
+        self.user = User.objects.create_user(username='user', password='password')
         self.recipe = Recipe.objects.create(
             name='Tea',
             description='Herbal',
             ingredients='Tea Leaves, Sugar, Water',
-            cooking_time='5'
+            cooking_time=5,
+            author=self.user
         )
 
     # Name test
@@ -27,6 +30,22 @@ class RecipeModelTest(TestCase):
 
         # compare the value to the expected result
         self.assertEqual(field_label, 'name')
+
+    def test_recipe_has_instructions_field(self):
+        field = self.recipe._meta.get_field('instructions')
+        self.assertEqual(field.verbose_name, 'instructions')
+
+    def test_recipe_has_image_field(self):
+        field = self.recipe._meta.get_field('image')
+        self.assertEqual(field.verbose_name, 'image')
+    
+    def test_recipe_has_difficulty_field(self):
+        field = self.recipe._meta.get_field('difficulty')
+        self.assertEqual(field.verbose_name, 'difficulty')
+
+    def test_recipe_has_author(self):
+        field = self.recipe._meta.get_field('author')
+        self.assertEqual(field.related_model.__name__, 'User')
     
     def test_calculate_difficulty_easy(self):
         recipe = Recipe(name='Tea', description='Herbal', ingredients='Tea Leaves, Sugar, Water', cooking_time=5)
@@ -44,40 +63,53 @@ class RecipeModelTest(TestCase):
         recipe = Recipe(name='Ramen', description='Fancy Soup', ingredients='Noodles, Water, Eggs, Corn, Shrimp, Chili Crisps ', cooking_time=20)
         self.assertEqual(recipe.calculate_difficulty(), 'Hard')
 
+    def test_save_sets_difficulty(self):
+        recipe = Recipe(
+            name='Tea',
+            description='Hot Beverage',
+            ingredients='Tea Leaves, Sugar, Water',
+            cooking_time=5,
+            author=self.user
+        )
+        recipe.save()
+        self.assertEqual(recipe.difficulty, 'Easy')
+
 # Recipe view/url tests
 class RecipeViewTests(TestCase):
 
     # Test data
     def setUp(self):
-        seld.recipe = Recipe.objects.create(
+        self.user = User.objects.create_user(username='user2', password='password')
+        self.recipe = Recipe.objects.create(
             name='Tea',
             description='Herbal',
             ingredients='Tea Leaves, Sugar, Water',
-            cooking_time='5'
+            cooking_time=5,
+            author=self.user
         )
     
     def test_recipe_list_url_exists(self):
-        response = self.client.get(reverse('recipe:recipe_list'))
+        response = self.client.get(reverse('recipes:recipe_list'))
         self.assertEqual(response.status_code, 200)
 
     def test_recipes_details_url_exists(self):
-        response = self.client.get(reverse('recipe:recipe_details', args=[self.recipe.id]))
+        response = self.client.get(reverse('recipes:recipe_details', args=[self.recipe.id]))
         self.assertEqual(response.status_code, 200)
 
     def test_recipe_list_template_used(self):
-        response = self.client.get(reverse('recipe_list'))
+        response = self.client.get(reverse('recipes:recipe_list'))
         self.assertTemplateUsed(response, 'recipes/recipe_list.html')
 
     def test_recipe_details_template_used(self):
-        response = self.client.get(reverse('recipe_details', args=[self.recipe.id]))
+        response = self.client.get(reverse('recipes:recipe_details', args=[self.recipe.id]))
         self.assertTemplateUsed(response, 'recipes/recipe_details.html')
 
     def test_recipe_list_contains_recipe(self):
-        response = self.client.get(reverse('recipe_list'))
+        response = self.client.get(reverse('recipes:recipe_list'))
         self.assertContains(response, "Tea")
 
     def test_recipe_details_contains_recipe_info(self):
-        response = self.client.get(reverse('recipe_details', args=[self.recipe.id]))
+        response = self.client.get(reverse('recipes:recipe_details', args=[self.recipe.id]))
         self.assertContains(response, 'Tea')
         self.assertContains(response, 'Tea Leaves')
 

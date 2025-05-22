a900b7c9b47e9df2fdb3480049c3e6eece16245a
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-05-22 19:25:10 -0400

    update recipes\urls.py to have a logout-success path, and create the success .html

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.recipes_home, name='recipes_home'), # Landing page
     path('recipes/', views.recipe_list, name='recipe_list'), # Full List
     path('recipes/<int:pk>/', views.recipe_details, name='recipe_details'), # Singular recipe details
+    path('logout-success/', views.logout_success, name='logout_success'), # Successful logout message
 ]

052d5cc10603a61c330739073736401ae39e7c2d
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-05-22 19:27:39 -0400

    add @login_required to desired views, add login and log out paths to the recipes\views.py, and redirects in settings.py fir the login and logout

--- src/recipe_project/urls.py
+++ src/recipe_project/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='recipes/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='recipes:logout_success'), name='logout'),
     path('', include('recipes.urls')),
 ]
 
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Recipe
 
 # Create your views here.
 
 # Home / Welcome page
 def recipes_home(request):
-    return render(request, 'recipes/recipes_home.html')
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes/recipes_home.html', {'recipes': recipes})
 
 # to display all recipes on a page
+@login_required
 def recipe_list(request): 
     recipes = Recipe.objects.all()
     return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
 
 # to show one recipes' details
+@login_required
 def recipe_details(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
     difficulty = recipe.calculate_difficulty()
     return render(request, 'recipes/recipe_details.html', {'recipe': recipe, 'difficulty': difficulty})
 
-
+# Log out success 
+def logout_success(request):
+    return render(request, 'recipes/success.html')

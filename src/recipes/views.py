755252aab15d20693554046067911fd2afde352d
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-06-21 11:57:31 -0400

    fixed typos and remove login required from recipes list

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Recipe
+from .forms import RecipeSearchForm
 
 # Create your views here.
 
@@ -10,7 +11,6 @@ def recipes_home(request):
     return render(request, 'recipes/recipes_home.html', {'recipes': recipes})
 
 # to display all recipes on a page
-@login_required
 def recipe_list(request): 
     recipes = Recipe.objects.all()
     return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
@@ -31,16 +31,16 @@ def login_success(request):
     return render(request, 'recipes/success.html')
 
 # search recipes
-def search_recipe(request):
+def recipe_search(request):
     form = RecipeSearchForm(request.GET or None)
-    recipe = Recipe.objects.all()
+    recipes = Recipe.objects.all()
 
     if form.is_valid():
         query = form.cleaned_data.get('query')
         food_type = form.cleaned_data.get('food_type')
 
         if query:
-            recipe = recipes.filter(name_icontains=query)
+            recipe = recipes.filter(name__icontains=query)
 
         if food_type:
             recipes = recipes.filter(food_type=food_type)
@@ -49,4 +49,4 @@ def search_recipe(request):
         'form' : form,
         'recipes' : recipes
     }
-    return render(request, 'recipes/recipe_search_html', context)
+    return render(request, 'recipes/recipe_search.html', context)

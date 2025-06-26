40469953e2dfd8b887c5a12f56707fc98d81939a
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-06-26 19:16:32 -0400

    Updated forms.py - added chart_choices for data analytics prep - added filters for search form

--- src/recipes/forms.py
+++ src/recipes/forms.py
@@ -1,6 +1,23 @@
 from django import forms
 from .models import FOOD_TYPES
 
+CHART_CHOICES = (
+    ('#1', 'Bar Chart'),
+    ('#2', 'Pie Chart'),
+    ('#3', 'Line Chart'),
+)
+
 class RecipeSearchForm(forms.Form):
     query = forms.CharField(label='Search Recipes', required=False)
+    ingredients = forms.CharField(required=False)
     food_type = forms.ChoiceField(choices=[('', 'All')] + FOOD_TYPES, required=False)
+    cooking_time = forms.IntegerField(label='Max Cooking Time', required=False)
+    difficulty = forms.ChoiceField(choices=[
+        ('', 'All'),
+        ('Easy', 'Easy'),
+        ('Intermediate', 'Intermediate'),
+        ('Moderate', 'Moderate'),
+        ('Hard', 'Hard'),
+    ], required=False)
+
+    chart_type = forms.ChoiceField(label='Chart Type', choices=CHART_CHOICES, required=False)

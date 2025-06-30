04a499e61891425ade71c3cf4c97d8fdd1500c19
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2025-06-30 18:23:10 -0400

    Updated views.py - imported get_chart from utils.py for chart visualization - added the logic to collect information, generate charts, and return the data broken down into chart format

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Recipe
 from .forms import RecipeSearchForm
+from .utils import get_chart
+import pandas as pd
 
 # Create your views here.
 
@@ -34,19 +36,47 @@ def login_success(request):
 def recipe_search(request):
     form = RecipeSearchForm(request.GET or None)
     recipes = Recipe.objects.all()
+    no_results = False
+    chart = None
+    table = None
 
     if form.is_valid():
-        query = form.cleaned_data.get('query')
+        name = form.cleaned_data.get('query')
+        ingredients = form.cleaned_data.get('ingredients')
         food_type = form.cleaned_data.get('food_type')
+        cooking_time = form.cleaned_data.get('cooking_time')
+        difficulty = form.cleaned_data.get('difficulty')
+        chart_type = form.cleaned_data.get('chart_type')
+        
+        recipes = Recipe.objects.all()
 
-        if query:
-            recipe = recipes.filter(name__icontains=query)
+        if name:
+            recipes = recipes.filter(name__icontains=name)
+
+        if ingredients:
+            recipes = recipes.filter(ingredients__icontains=ingredients)
 
         if food_type:
             recipes = recipes.filter(food_type=food_type)
 
+        if cooking_time:
+            recipes = recipes.filter(cooking_time__lte=cooking_time)
+
+        if difficulty:
+            recipes = recipes.filter(difficulty=difficulty)
+
+        if not recipes.exists(): 
+            no_results = True
+        else:
+            df = pd.DataFrame(recipes.values('name', 'ingredients', 'food_type', 'cooking_time', 'difficulty'))
+            table = df.to_html()
+            chart = get_chart(chart_type, df)
+
     context = {
         'form' : form,
-        'recipes' : recipes
+        'recipes' : recipes,
+        'no_results' : no_results,
+        'chart': chart,
+        'table': table if not no_results else None
     }
     return render(request, 'recipes/recipe_search.html', context)
